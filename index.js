dfaacb868ff05e8c7953e1c05aeb37f3f7178ba6
sunday <142508130+djssunday@users.noreply.github.com>
2023-10-22 23:55:12 +0000

    update

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import fetch from "node-fetch";
 
 var getData = async () => {
-  var response = await fetch("https://www.binance.com/api/v3/exchangeInfo");
+  var response = await fetch("https://fapi.binance.com/fapi/v1/exchangeInfo");
   let data = await response.json();
   let symbols = data.symbols
     .filter(
       (x) =>
         x.status == "TRADING" &&
-        (x.quoteAsset == "BUSD" || x.quoteAsset == "USDT"),
-    )
+        x.contractType == "PERPETUAL"  
+      )
     .filter((value, index, self) => self.map(x => x.baseAsset).indexOf(value.baseAsset) == index) 
     .map((x) => x.symbol)
     .sort()
