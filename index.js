0bec847d85a00eac6908192c42710c7d4058c90a
sunday <142508130+djssunday@users.noreply.github.com>
2023-11-03 03:18:17 +0000

    update

--- index.js
+++ index.js
@@ -18,3 +18,22 @@ var getData = async () => {
 };
 getData();
 // console.log(response);
+//
+
+var getData2 = async () => {
+  var response = await fetch("https://dapi.binance.com/dapi/v1/exchangeInfo");
+  let data = await response.json();
+  let symbols = data.symbols
+    .filter(
+      (x) =>
+        x.contractStatus== "TRADING" &&
+        x.contractType == "PERPETUAL"  
+      )
+    .filter((value, index, self) => self.map(x => x.baseAsset).indexOf(value.baseAsset) == index) 
+    .map((x) => `BINANCE:${x.baseAsset}${x.quoteAsset}.P`)
+    .sort();
+  
+  symbols.forEach(x => console.log(x));
+};
+
+getData2();
